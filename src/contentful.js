37b71d4e96fc7781d454d9c23a057bdeb621ddf9
josekuaan <joshuaifeanyi67@gmail.com>
2020-03-06 07:22:18 -0800

    fixing react environment variable

--- src/contentful.js
+++ src/contentful.js
@@ -1,11 +1,10 @@
 import { createClient } from 'contentful'
-const  dotenv = require('dotenv')
-          let env = dotenv.config()
 
 
  export default createClient({
- accessToken: process.env.REACT._APP_API_ACCESS_TOKEN,
- space: process.env.REACT_APP_SPACE
+    space: process.env.REACT_APP_SPACE,
+ accessToken: process.env.REACT._APP_API_ACCESS_TOKEN
+
  
 })
 
